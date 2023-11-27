c1772adfd131aa4b3eaaa851fb25bdaeb06125ce
Joe <Jhuntenburg@gmail.com>
2023-11-27 11:42:41 -0500

    spring boot initialized and inital commit test

---
+++ src/main/java/com/example/seniorfullstackdyhtwb/SeniorFullStackDyhtwbApplication.java
@@ -0,0 +1,13 @@
+package com.example.seniorfullstackdyhtwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorFullStackDyhtwbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeniorFullStackDyhtwbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seniorfullstackdyhtwb/SeniorFullStackDyhtwbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seniorfullstackdyhtwb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorFullStackDyhtwbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
