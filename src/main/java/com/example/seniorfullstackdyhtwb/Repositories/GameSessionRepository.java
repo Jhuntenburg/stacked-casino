c61f469e21986d3057db88db864cf8fbbf7d8b65
Joe <Jhuntenburg@gmail.com>
2023-11-29 13:54:59 -0500

    upload to personal repo

--- src/main/java/com/example/seniorfullstackdyhtwb/Repositories/GameSessionRepository.java
+++ src/main/java/com/example/seniorfullstackdyhtwb/Repositories/GameSessionRepository.java
@@ -4,4 +4,5 @@ import com.example.seniorfullstackdyhtwb.Model.GameSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
+
 }
